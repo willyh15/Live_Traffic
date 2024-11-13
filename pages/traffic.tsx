8837e2f4254c748319cc7e2cbadd18c839c7c56a
Will Harrison <willharrison2016@gmail.com>
2024-11-13 14:10:52 -0600

    Update traffic.tsx

---
+++ pages/traffic.tsx
@@ -0,0 +1,10 @@
+import TrafficUpdates from '../src/components/TrafficUpdates';
+
+const TrafficPage = () => (
+  <div>
+    <h1>UPS Driver Traffic Dashboard</h1>
+    <TrafficUpdates />
+  </div>
+);
+
+export default TrafficPage;
